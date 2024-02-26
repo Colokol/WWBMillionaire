813af24658de1393ff531143e598feb04a7e1b8e
Colokol <Colokol1@gmail.com>
2024-02-26 10:32:40 +0300

    Initial Commit

---
+++ Who Wants to Be a Millionaire/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Who Wants to Be a Millionaire
+//
+//  Created by Uladzislau Yatskevich on 26.02.24.
+//
+
+import Foundation
---
+++ Who Wants to Be a Millionaire/Modules/MainScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Who Wants to Be a Millionaire
+//
+//  Created by Uladzislau Yatskevich on 26.02.24.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
