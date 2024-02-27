7d04fa0835a7c42787d69f90e35e6776482eac6f
Alexander Altman <qewhouse@gmail.com>
2024-02-27 23:52:07 +0300

    + launch screen

---
+++ Who Wants to Be a Millionaire/Modules/MainScreenVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainScreenVC.swift
+//  Who Wants to Be a Millionaire
+//
+//  Created by Alexander Altman on 27.02.2024.
+//
+
+import UIKit
+
+final class MainScreenVC: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+}
